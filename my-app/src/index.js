a8933f9131c6259743365dcdc66ec2d605d13980
sanyokstd <sanyokstd@gmail.com>
2022-02-22 20:55:43 +0200

    1
    1

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,24 +1,22 @@
+import * as serviceWorker from './serviceWorker';
+import store from "./redux/redux-store";
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-import store from './redux/redux-store';
-
-import {Provider} from 'react-redux';
-
+import {BrowserRouter} from "react-router-dom";
+import {Provider} from "react-redux";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+    <BrowserRouter>
+        <Provider store={store}>
+            <App/>
+        </Provider>
+    </BrowserRouter>, document.getElementById('root'));
 
 
-
-store.subscribe(()=>{
-  let state = store.getState()
-})
+// API
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister();
--- my-app/src/redux/profile-reducer.js
+++ my-app/src/redux/profile-reducer.js
@@ -1,46 +1,51 @@
-const ADD_POST = 'ADD_POST'
-const UPDATE_NEW_POST_TEXT = 'UPDATE_NEW_POST_TEXT'
+const ADD_POST = 'ADD-POST';
+const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT';
+const SET_USER_PROFILE = 'SET_USER_PROFILE';
 
-let initState = {
-    postsData: [
-        {id: 1, mess: 'mess1', like: 15},
-        {id: 2, mess: 'mess2', like: 20}
+let initialState = {
+    posts: [
+        {id: 1, message: 'Hi, how are you?', likesCount: 12},
+        {id: 2, message: 'It\'s my first post', likesCount: 11},
+        {id: 3, message: 'Blabla', likesCount: 11},
+        {id: 4, message: 'Dada', likesCount: 11}
     ],
-    newPostText: "new post"
-}
+    newPostText: 'it-kamasutra.com',
+    profile: null
+};
 
-const profileReducer = (state = initState, action) => {
-    switch(action.type){
+const profileReducer = (state = initialState, action) => {
+
+    switch(action.type) {
         case ADD_POST: {
-            let newPost ={
+            let newPost = {
                 id: 5,
-                mess: state.newPostText,
-                like: 0
-            }
-
-            let stateCopy = {...state};
-            stateCopy.postsData = [...state.postsData]
-            stateCopy.postsData.push(newPost);
-            stateCopy.newPostText = ''
-            return stateCopy;
+                message: state.newPostText,
+                likesCount: 0
+            };
+            return {
+                ...state,
+                posts: [...state.posts, newPost],
+                newPostText: ''
+            };
         }
-        case UPDATE_NEW_POST_TEXT:{
-            let stateCopy = {...state}
-            stateCopy.newPostText = action.newText;
-            return stateCopy;
-        }   
+        case UPDATE_NEW_POST_TEXT: {
+            return {
+                ...state,
+                newPostText: action.newText
+            }
+        }
+        case SET_USER_PROFILE: {
+            return {...state, profile: action.profile}
+        }
         default:
-            return state;    
+            return state;
     }
 }
 
-//action creators
-export const addPostActionCreator = () =>({type: ADD_POST})
 
-export const updateNewPostTextActionCreator = (text) => (
-    {
-        type: UPDATE_NEW_POST_TEXT,
-        newText: text
-    }
-)
+export const addPostActionCreator = () => ({type: ADD_POST})
+export const setUserProfile = (profile) => ({type: SET_USER_PROFILE, profile})
+export const updateNewPostTextActionCreator = (text) =>
+    ({type: UPDATE_NEW_POST_TEXT, newText: text })
+
 export default profileReducer;
